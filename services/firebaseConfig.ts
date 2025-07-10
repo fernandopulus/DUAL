63c762b59b1a47d117b38e04c4e349cdb0c73f5a
fernandopulus <fernando.sagredo@industrialderecoleta.cl>
2025-07-10 03:10:28 +0000

    Eliminar archivos innecesarios

---
+++ services/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYJ8aGffq3DoTyx-MKHDLqKTzqJVIrdws",
+  authDomain: "dualgrade.firebaseapp.com",
+  projectId: "dualgrade",
+  storageBucket: "dualgrade.firebasestorage.app",
+  messagingSenderId: "588826913037",
+  appId: "1:588826913037:web:c4b6ae2b113d027633a844"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
