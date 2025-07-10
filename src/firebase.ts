3cd973e175140b752a05cd6c50ddaad3242ba79a
fernandopulus <fernando.sagredo@industrialderecoleta.cl>
2025-07-10 17:13:15 +0000

    firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYJ8aGffq3DoTyx-MKHDLqKTzqJVIrdws",
+  authDomain: "dualgrade.firebaseapp.com",
+  projectId: "dualgrade",
+  storageBucket: "dualgrade.firebasestorage.app",
+  messagingSenderId: "588826913037",
+  appId: "1:588826913037:web:65d0b141a93b8c6233a844"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
